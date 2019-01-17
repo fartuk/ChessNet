368be9842f3767142dc48c19964debbfd35d5ea7
Artur Fattakhov <fattakhov@unim.su>
2019-01-17 23:12:09 +0100

    working loader

--- loader.py
+++ loader.py
@@ -3,21 +3,20 @@ import pandas as pd
 import os
 import chess
 import chess.pgn
-from board_utils import get_position_tensor, flip_move, get_position, move2tensor
+from board_utils import get_position_tensor, flip_move, get_position, move2tensor, ohe_board
 
 
 def keras_generator(batch_size=32, min_elo=2900):
     while(1):
-        x_batch = []
-        y_batch = []
-        
         pgns_path = 'data/pgns/40_40/'
         pgn_name = np.random.choice(os.listdir(pgns_path))  
         
         pgn = open(pgns_path + pgn_name)
         game = chess.pgn.read_game(pgn)
         while game is not None:
-                
+            x_batch = []
+            y_batch = []
+        
             if int(game.headers['BlackElo']) > min_elo and int(game.headers['WhiteElo']) > min_elo:
                 white = np.random.choice([False, True])
             elif max(int(game.headers['BlackElo']), int(game.headers['WhiteElo'])) > min_elo:
@@ -45,4 +44,5 @@ def keras_generator(batch_size=32, min_elo=2900):
             y_batch = np.array(y_batch)
 
             yield x_batch, y_batch
+
         
