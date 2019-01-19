540110d6433da6cfb17d5ef26c156fce90b7eccc
Artur Fattakhov <fattakhov@unim.su>
2019-01-19 16:18:52 +0100

    sep notebooks

--- loader.py
+++ loader.py
@@ -16,29 +16,31 @@ def keras_generator(batch_size=32, min_elo=2900):
         while game is not None:
             x_batch = []
             y_batch = []
-        
-            if int(game.headers['BlackElo']) > min_elo and int(game.headers['WhiteElo']) > min_elo:
-                white = np.random.choice([False, True])
-            elif max(int(game.headers['BlackElo']), int(game.headers['WhiteElo'])) > min_elo:
-                white = bool(np.argmax([int(game.headers['BlackElo']), int(game.headers['WhiteElo'])]))
-            else:
-                break
-                
-            for move_id in np.random.choice(list(range(1 - int(white), int(game.headers['PlyCount']) - 1, 2)), batch_size):
-                position = get_position(game, move_id)
-                move_str = position.move.__str__()
-                feat_tensor = get_position_tensor(position.parent.board(), white=white)
-                if not white:
-                    move_str = flip_move(move_str)
+            try:
+                if int(game.headers['BlackElo']) > min_elo and int(game.headers['WhiteElo']) > min_elo:
+                    white = np.random.choice([False, True])
+                elif max(int(game.headers['BlackElo']), int(game.headers['WhiteElo'])) > min_elo:
+                    white = bool(np.argmax([int(game.headers['BlackElo']), int(game.headers['WhiteElo'])]))
+                else:
+                    break
 
-                move_tensor = move2tensor(move_str)
-                assert move_tensor.sum() == 1
-                
-                x_batch += [feat_tensor]
-                y_batch += [move_tensor]
+                for move_id in np.random.choice(list(range(1 - int(white), int(game.headers['PlyCount']) - 1, 2)), batch_size):
+                    position = get_position(game, move_id)
+                    move_str = position.move.__str__()
+                    feat_tensor = get_position_tensor(position.parent.board(), white=white)
+                    if not white:
+                        move_str = flip_move(move_str)
+
+                    move_tensor = move2tensor(move_str)
+                    assert move_tensor.sum() == 1
+
+                    x_batch += [feat_tensor]
+                    y_batch += [move_tensor]
+            except:
+                    game = chess.pgn.read_game(pgn)
+                    continue
                 
             game = chess.pgn.read_game(pgn)
-
 
             x_batch = np.array(x_batch)
             y_batch = np.array(y_batch)
